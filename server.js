dc544aac77a5fb175e28925f389985203fd16f73
Mikolas Bronec <24sbronec@dgkralupy.eu>
2024-12-06 15:50:38 +0100

    1/2 HTML

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
-const db = require('./src/database');
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+const db = require("./src/database");
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
